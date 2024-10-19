08e26619d499c96cee6b2a1649c6016017914230
vraz <vraz@pipmail.net>
2024-10-19 17:02:52 -0500

    Added cookout message for extension cookie query

--- launcher/plog.c
+++ launcher/plog.c
@@ -26,19 +26,30 @@ struct plog_source {
     CogView *view;
 };
 
+static void
+print_module_info(GIOExtension *extension, void *userdata G_GNUC_UNUSED)
+{
+    g_info("  %s - %d/%s",
+           g_io_extension_get_name(extension),
+           g_io_extension_get_priority(extension),
+           g_type_name(g_io_extension_get_type(extension)));
+}
+
 /* check if player pipe needs service */
-static gboolean plog_check(GSource *state)
+static gboolean
+player_check(GSource *state)
 {
     struct plog_source *source = (void *)state;
     return(!!source->pfd.revents);
 }
 
 /* get player progress updates and handle player termination */
-static gboolean plog_dispatch(GSource *state, GSourceFunc callback, gpointer user_data)
+static gboolean
+player_dispatch(GSource *state, GSourceFunc callback, gpointer user_data)
 {
     struct plog_source *source = (void *)state;
     WebKitWebView *web_view = (WebKitWebView *)source->view;
-    g_print("plog_dispatch\n");
+    g_print("player_dispatch\n");
 
     if (!(source->pfd.revents & (G_IO_ERR|G_IO_HUP))) {
         /* process child status updates */
@@ -78,22 +89,14 @@ static gboolean plog_dispatch(GSource *state, GSourceFunc callback, gpointer use
     return(FALSE);
 }
 
-static GSourceFuncs plog_funcs = {
-    .check = plog_check,
-    .dispatch = plog_dispatch,
+static GSourceFuncs player_funcs = {
+    .check = player_check,
+    .dispatch = player_dispatch,
 };
 
+/* process javascript player message */
 static void
-print_module_info(GIOExtension *extension, void *userdata G_GNUC_UNUSED)
-{
-    g_info("  %s - %d/%s",
-           g_io_extension_get_name(extension),
-           g_io_extension_get_priority(extension),
-           g_type_name(g_io_extension_get_type(extension)));
-}
-
-// process javascript player message
-static void on_message(WebKitUserContentManager *content, WebKitJavascriptResult *result, void *view)
+on_message(WebKitUserContentManager *content, WebKitJavascriptResult *result, void *view)
 {
     JSCValue *jsc = webkit_javascript_result_get_js_value(result);
     const gchar *tmpl = g_getenv("PLOG_PLAYER");
@@ -167,7 +170,7 @@ static void on_message(WebKitUserContentManager *content, WebKitJavascriptResult
         close(pipefd[1]);
 
     /* create player-source to monitor playback */
-    struct plog_source *source = (void *)g_source_new(&plog_funcs, sizeof(struct plog_source));
+    struct plog_source *source = (void *)g_source_new(&player_funcs, sizeof(struct plog_source));
     source->view = view;
     source->pid = pid;
     source->elap = g_get_monotonic_time();
@@ -181,25 +184,54 @@ static void on_message(WebKitUserContentManager *content, WebKitJavascriptResult
     g_source_attach(&source->s, g_main_context_get_thread_default());
 }
 
-static void on_load_changed(WebKitWebView *web_view, WebKitLoadEvent event, void *userdata)
+static void
+on_cookie_get(WebKitCookieManager *manager, GAsyncResult *result, void *userdata)
 {
-    const gchar *uri = webkit_web_view_get_uri(web_view);
-    g_debug("plog: on_load_changed: %s\n", uri);
+  WebKitWebView *view = userdata;
+
+  GString *jar = g_string_new(NULL);
+  GList *list = webkit_cookie_manager_get_cookies_finish(manager, result, NULL);
+  for (GList *p = list; p != NULL; p = p->next) {
+    SoupCookie *c = p->data;
+    gchar *hdr = soup_cookie_to_set_cookie_header(c);
+    g_string_append_printf(jar, "%s\n", hdr);
+    g_free(hdr);
+  }
+  g_list_free(list);
+
+  WebKitUserMessage *msg = webkit_user_message_new("cookout", g_variant_new("s", jar->str));
+  g_string_free(jar, TRUE);
+  webkit_web_view_send_message_to_page(view, msg, NULL, NULL, NULL);
 }
 
-static void on_activate(GApplication *self, void *user_data)
+static gboolean
+on_view_msg(WebKitWebView *view, WebKitUserMessage *msg, void *userdata)
+{
+  const gchar *uri = webkit_web_view_get_uri(view);
+  const gchar *name = webkit_user_message_get_name(msg);
+
+  g_print("on_view_msg: %s %s\n", name, uri);
+
+  if (strcmp(name, "cookout") == 0) {
+    WebKitWebContext *ctx = webkit_web_view_get_context(view);
+    WebKitCookieManager *cm = webkit_web_context_get_cookie_manager(ctx);
+    webkit_cookie_manager_get_cookies(cm, uri, NULL, (GAsyncReadyCallback)on_cookie_get, view);
+  }
+  return(FALSE);
+}
+
+static void
+on_activate(GApplication *self, void *user_data)
 {
     CogLauncher *launcher = COG_LAUNCHER(self);
     CogViewport *viewport = cog_launcher_get_viewport(launcher);
     WebKitWebView *view = (WebKitWebView *)cog_viewport_get_visible_view(viewport);
+
     WebKitUserContentManager *content = webkit_web_view_get_user_content_manager(view);
     g_signal_connect(content, "script-message-received::plog", G_CALLBACK(on_message), view);
     webkit_user_content_manager_register_script_message_handler(content, "plog");
 
-    //CogShell *shell = cog_launcher_get_shell(launcher);
-    //WebKitWebContext *webctx = cog_shell_get_web_context(shell);
-    //CogView *view = cog_viewport_get_visible_view(viewport);
-    g_signal_connect(view, "load-changed", G_CALLBACK(on_load_changed), NULL);
+    g_signal_connect(view, "user-message-received", G_CALLBACK(on_view_msg), NULL);
 }
 
 int main(int argc, char *argv[])
@@ -212,3 +244,4 @@ int main(int argc, char *argv[])
     g_signal_connect(app, "activate", G_CALLBACK(on_activate), NULL);
     return(g_application_run(app, argc, argv));
 }
+
